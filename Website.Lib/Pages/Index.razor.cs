8a9c4d50b02ddbbc3b5595f5af0d0d071d0e69bd
Simon Ziegler <simon@dioptra.tech>
2022-04-12 19:33:13 +0100

    Intermediate

---
+++ Website.Lib/Pages/Index.razor.cs
@@ -0,0 +1,13 @@
+﻿namespace Website.Lib.Pages;
+
+using Material.Blazor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public partial class Index
+{
+}
+
