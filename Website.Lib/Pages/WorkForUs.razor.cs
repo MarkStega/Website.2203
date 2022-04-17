0573951d6792c752bae91c5c5668fb36e4f50727
Simon Ziegler <simon@dioptra.tech>
2022-04-17 09:18:32 +0100

    Home button complete

---
+++ Website.Lib/Pages/WorkForUs.razor.cs
@@ -0,0 +1,8 @@
+﻿using Material.Blazor;
+using Microsoft.AspNetCore.Components;
+
+namespace Website.Lib.Pages;
+public partial class WorkForUs : ComponentBase
+{
+    private bool ShowStuff { get; set; } = false;
+}
