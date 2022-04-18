e4c4ca224423004ae9a4996bbb7b00cf75413ed7
Simon Ziegler <simon@dioptra.tech>
2022-04-18 19:58:48 +0100

    EoD

--- Website.Lib/Pages/Index.razor.cs
+++ Website.Lib/Pages/Index.razor.cs
@@ -4,9 +4,26 @@ using Website.Lib.Shared;
 namespace Website.Lib.Pages;
 public partial class Index : ComponentBase
 {
+    private class CarouselDataType
+    {
+        public string HoldingText { get; set; } = "";
+        public string Caption { get; set; } = "";
+    }
+
+
     [Inject] private NavigationManager NavigationManager { get; set; }
 
     [CascadingParameter] private Action<bool> ShowHomeButton { get; set; }
+
+
+    private static readonly CarouselDataType[] CarouselData = new CarouselDataType[]
+    {
+        new() { HoldingText = "Panel 1", Caption = "Caption 1" },
+        new() { HoldingText = "Panel 2", Caption = "Caption 2" },
+        new() { HoldingText = "Panel 3", Caption = "Caption 3" },
+        new() { HoldingText = "Panel 4", Caption = "Caption 4" },
+        new() { HoldingText = "Panel 5", Caption = "Caption 5" },
+    };
 
 
     protected override void OnInitialized()
@@ -14,15 +31,6 @@ public partial class Index : ComponentBase
         base.OnInitialized();
 
         ShowHomeButton(false);
-
-        /*CarouselData = new()
-        {
-            new() { Caption = "Panel 1" },
-            new() { Caption = "Panel 2" },
-            new() { Caption = "Panel 3" },
-            new() { Caption = "Panel 4" },
-            new() { Caption = "Panel 5" },
-        };*/
     }
 
 
