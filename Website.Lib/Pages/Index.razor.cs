30bfab5f9d1545735c0e9ff67c787d05a4f04d56
Simon Ziegler <simon@dioptra.tech>
2022-04-18 14:45:27 +0100

    Minor updates

--- Website.Lib/Pages/Index.razor.cs
+++ Website.Lib/Pages/Index.razor.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Website.Lib.Shared;
 
 namespace Website.Lib.Pages;
 public partial class Index : ComponentBase
@@ -8,12 +9,20 @@ public partial class Index : ComponentBase
     [CascadingParameter] private Action<bool> ShowHomeButton { get; set; }
 
 
-
     protected override void OnInitialized()
     {
         base.OnInitialized();
 
         ShowHomeButton(false);
+
+        /*CarouselData = new()
+        {
+            new() { Caption = "Panel 1" },
+            new() { Caption = "Panel 2" },
+            new() { Caption = "Panel 3" },
+            new() { Caption = "Panel 4" },
+            new() { Caption = "Panel 5" },
+        };*/
     }
 
 
