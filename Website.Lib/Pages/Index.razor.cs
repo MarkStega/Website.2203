b1f2e90e99a0a39551e6845c99bc53a072364298
Simon Ziegler <simon@dioptra.tech>
2022-04-19 23:56:55 +0100

    EoD

--- Website.Lib/Pages/Index.razor.cs
+++ Website.Lib/Pages/Index.razor.cs
@@ -19,12 +19,12 @@ public partial class Index : ComponentBase
     private static readonly CarouselDataType[] CarouselData = new CarouselDataType[]
     {
         new() { Uri = "_content/Website.Lib/images/01-main-screen.webp", Caption = "Dioptra's main screen layout" },
-        new() { Uri = "_content/Website.Lib/images/02-main-screen-search.webp", Caption = "Searching for a scheme, sorted by quantitative risk evaluation" },
+        new() { Uri = "_content/Website.Lib/images/02-main-screen-search.webp", Caption = "Scheme search" },
         new() { Uri = "_content/Website.Lib/images/03-march-costs-chart.webp", Caption = "Budget, actual and forecast development costs" },
         new() { Uri = "_content/Website.Lib/images/04-march-accruals.webp", Caption = "Loan interest accrual details" },
         new() { Uri = "_content/Website.Lib/images/05-march-cost-summary.webp", Caption = "Project cost summary" },
-        new() { Uri = "_content/Website.Lib/images/06-march-capital-flows.webp", Caption = "Chart of blended actual/forecast capital structure evolution over time" },
-        new() { Uri = "_content/Website.Lib/images/07-march-land-reg.webp", Caption = "UK Land Registry sold unit prices mapped and individually detailed" },
+        new() { Uri = "_content/Website.Lib/images/06-march-capital-flows.webp", Caption = "Actual/forecast capital structure" },
+        new() { Uri = "_content/Website.Lib/images/07-march-land-reg.webp", Caption = "UK Land Registry sold unit prices" },
         new() { Uri = "_content/Website.Lib/images/08-march-version-graph.webp", Caption = "Scheme data versioning/audit" },
         new() { Uri = "_content/Website.Lib/images/09-march-edit-budget-schedule.webp", Caption = "Editting cost budget schedules" },
     };
